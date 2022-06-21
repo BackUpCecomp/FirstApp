e5c3a368504fb1e3d90904f18d92b769bbe61aca
MeloP223 <backupcecomp21@gmail.com>
2022-06-21 10:23:52 -0500

    first commit

---
+++ app/src/main/java/com/example/myfirstapp/UI/InicioActivity.kt
@@ -0,0 +1,12 @@
+package com.example.myfirstapp.UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.myfirstapp.R
+
+class InicioActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
